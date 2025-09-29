0ec57669f6b1c4def887778938933bdeda0aad55
hahuaja <hahujahawiwiwi@gmail.com>
2025-09-29 14:30:25 +0700

    Add files via upload

---
+++ 99nighthub.lua
@@ -0,0 +1,264 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "V4N HUB🕸️",
+   Icon = 0,
+   LoadingTitle = "V4N HUB LOAD 🕸️",
+   LoadingSubtitle = "🕸️",
+   ShowText = "CODED BY EVAN",
+   Theme = "AmberGlow",
+   ToggleUIKeybind = "K",
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
+   KeySystem = true,
+   KeySettings = {
+      Title = "VNZ HUB",
+      Subtitle = "KEY",
+      Note = "ENTER KEY",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"FREE2025VANZ"}
+   }
+})
+
+Rayfield:Notify({
+   Title = "CREDIT: EVAN",
+   Content = "Universal hub",
+   Duration = 6.5,
+   Image = 4483362458,
+})
+
+local NightTab = Window:CreateTab("99 night", 4483362458)
+NightTab:CreateSection("select 99 night script")
+
+NightTab:CreateButton({
+   Name = "INVISIBLE ONLY 99 NIGHT",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/rIgRPFcD/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "auto open chest[Beta]",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/fgF11kEz/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "Kill aura",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastebin.com/raw/CXsrLzJ9'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "one click proximityprompt",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/efg96qjg/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "skybase maker",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/fINcylMo/raw'))()
+   end,
+})
+local Divider = NightTab:CreateDivider()
+NightTab:CreateSection("bring")
+NightTab:CreateButton({
+   Name = "bring fuel Fix",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/fLilAZps/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "auto sapling(must have sapling)",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/Rku4viPt/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "manual sapling(must have sapling)",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/7v8847Xm/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "Bring food🍽️[Fix!]",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastebin.com/raw/zq5BAj0g'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "bring scrap/besi fix[UPDATE]",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/a0VcBcbW/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "Bring Bandage👾",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/yYvGHrRx/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "Bring Logs to crafting(beta,newfiture)🪵",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/O4IozY2I/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "Bring scrap to crafting(beta,newfiture)",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/71TQ1mgq/raw'))()
+   end,
+})
+
+NightTab:CreateButton({
+   Name = "Leveling up campfire(BETA,NEWFITUR)🔥🔥",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/X0WcD1KR/raw'))()
+   end,
+})
+local Divider = NightTab:CreateDivider()
+NightTab:CreateSection("TELEPORT")
+NightTab:CreateButton({
+   Name = "teleport campfire",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/M2WGEJZ3/raw'))()
+   end,
+})
+
+local function createLostChildTeleport(name)
+   NightTab:CreateButton({
+      Name = "teleport " .. name,
+      Callback = function()
+         loadstring(game:HttpGet('https://pastefy.app/wXQmeKWR/raw'))()
+      end,
+   })
+end
+
+createLostChildTeleport("Lost Child")
+createLostChildTeleport("Lost Child2")
+createLostChildTeleport("Lost Child3")
+createLostChildTeleport("Lost Child4")
+
+local UniversalTab = Window:CreateTab("Universal", 4483362458)
+UniversalTab:CreateSection("select universal script")
+
+UniversalTab:CreateButton({
+   Name = "fly",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/WFjlUMzj/raw'))()
+   end,
+})
+
+UniversalTab:CreateButton({
+   Name = "inf jump",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/n9n5bStX/raw'))()
+   end,
+})
+
+UniversalTab:CreateButton({
+   Name = "Invisible universal",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/Ee5pbTPy/raw'))()
+   end,
+})
+
+UniversalTab:CreateButton({
+   Name = "double jump",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/dO9rF0Ig/raw'))()
+   end,
+})
+
+UniversalTab:CreateButton({
+   Name = "noclip",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/b33Ogb6c/raw'))()
+   end,
+})
+
+UniversalTab:CreateButton({
+   Name = "outline",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/WwBPG3cN/raw'))()
+   end,
+})
+
+UniversalTab:CreateButton({
+   Name = "TELEPORT",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/tlOmXiCf/raw'))()
+   end,
+})
+
+UniversalTab:CreateButton({
+   Name = "AIMLOCK",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/qKYnrNwl/raw'))()
+   end,
+})
+
+local TrollTab = Window:CreateTab("Troll", 4483362458)
+TrollTab:CreateButton({
+   Name = "peluk/hug",
+   Callback = function()
+      loadstring(game:HttpGet("https://pastefy.app/XE8YCI4U/raw"))()
+   end,
+})
+
+local AnimationTab = Window:CreateTab("Animation", 4483362458)
+AnimationTab:CreateSection("Cannot select more than 1 animation unless you leave it first and select it again.")
+
+AnimationTab:CreateButton({
+   Name = "Animation Adidas community only r15",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/0KvW2Nmk/raw'))()
+   end,
+})
+
+AnimationTab:CreateButton({
+   Name = "Animation ninja r15",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/PmaHpzM0/raw'))()
+   end,
+})
+
+AnimationTab:CreateButton({
+   Name = "Animation bubbly r15",
+   Callback = function()
+      loadstring(game:HttpGet('https://pastefy.app/nJ3Qjs8V/raw'))()
+   end,
+})
+
+local InfoTab = Window:CreateTab("INFO", 4483362458)
+InfoTab:CreateLabel([[✨ Evan Hub v1.04
+We’re excited to announce that Evan Hub has officially been updated to version 3.0!
+]])
+InfoTab:CreateLabel([[This update brings improved stability, cleaner design, and better overall performance.
+Stay tuned — more powerful features are coming soon! 🚀]])
+
+Rayfield:LoadConfiguration()
